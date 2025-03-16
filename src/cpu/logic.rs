d54b2b6b9089ff0c7c5558319fa2f76ce494caaa
Ryan Douglas <rcdouglas@crimson.ua.edu>
2025-03-16 13:25:57 -0500

    INX Implementation

--- src/cpu/logic.rs
+++ src/cpu/logic.rs
@@ -1,5 +1,6 @@
 pub struct CPU {
     pub register_a: u8,
+    pub register_x: u8,
     pub status: u8,
     pub program_counter: u16,
 }
@@ -8,11 +9,40 @@ impl CPU {
     pub fn new() -> Self {
         CPU {
             register_a: 0,
+            register_x: 0,
             status: 0,
             program_counter: 0,
         }
     }
 
+    fn lda(&mut self, value: u8) {
+        self.register_a = value;
+        self.update_zero_and_negative_flags(self.register_a);
+    }
+
+    fn tax(&mut self) {
+        self.register_x = self.register_a;
+        self.update_zero_and_negative_flags(self.register_x);
+    }
+
+    fn inx(&mut self) {
+        self.register_x = self.register_x.wrapping_add(1);
+        self.update_zero_and_negative_flags(self.register_x);
+    }
+
+    fn update_zero_and_negative_flags(&mut self, result: u8) {
+        if result == 0 {
+            self.status |= 0b0000_0010;
+        } else {
+            self.status &= 0b1111_1101;
+        }
+
+        if result & 0b1000_0000 != 0 {
+            self.status |= 0b1000_0000;
+        } else {
+            self.status &= 0b0111_1111;
+        }
+    }
     pub fn interpret(&mut self, program: Vec<u8>) {
         self.program_counter = 0;
 
@@ -25,22 +55,16 @@ impl CPU {
                 0xA9 => {
                     let param = program[self.program_counter as usize];
                     self.program_counter += 1;
-                    self.register_a = param;
-
-                    // Zero Flag
-                    if self.register_a == 0 {
-                        self.status = self.status | 0b0000_0010;
-                    } else {
-                        self.status = self.status & 0b1111_1101;
-                    }
-
-                    // Negative Flag
-                    if self.register_a & 0b1000_0000 != 0 {
-                        self.status = self.status | 0b1000_0000;
-                    } else {
-                        self.status = self.status & 0b0111_1111;
-                    }
+                    
+                    self.lda(param);
                 }
+
+                // TAX - Transfer Accumulator to X
+                0xAA => self.tax(),
+
+                // INX - Increment X Register
+                0xE8 => self.inx(),
+
                 // BRK - Break
                 0x00 => {
                     return;
--- src/cpu/tests.rs
+++ src/cpu/tests.rs
@@ -4,6 +4,7 @@ use crate::cpu::logic::CPU;
 mod test {
     use super::*;
 
+    // 0xA9 LDA - Load Accumulator
     #[test]
     fn test_0xa9_lda_immediate_load_data() {
         let mut cpu = CPU::new();
@@ -25,5 +26,34 @@ mod test {
         let mut cpu = CPU::new();
         cpu.interpret(vec![0xa9, 0xff, 0x00]);
         assert!(cpu.status & 0b1000_0000 == 0b1000_0000);
+    }
+
+    // 0xAA TAX - Transfer Accumulator to X
+    #[test]
+    fn test_0xaa_tax_move_a_to_x() {
+        let mut cpu = CPU::new();
+        cpu.interpret(vec![0xa9, 0x05, 0xaa, 0x00]);
+        assert_eq!(cpu.register_x, 0x05);
+        assert!(cpu.status & 0b0000_0010 == 0b00);
+        assert!(cpu.status & 0b1000_0000 == 0);
+    }
+
+    // 0xE8 INX - Increment X Register
+    #[test]
+    fn test_inx_overflow() {
+        let mut cpu = CPU::new();
+        cpu.register_x = 0xff;
+        cpu.interpret(vec![0xe8, 0xe8, 0x00]);
+
+        assert_eq!(cpu.register_x, 1)
+    }
+
+    // LDAs, TAX, and INX working together
+    #[test]
+    fn test_5_ops_working_together() {
+        let mut cpu = CPU::new();
+        cpu.interpret(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]);
+
+        assert_eq!(cpu.register_x, 0xc1)
     }
 }
